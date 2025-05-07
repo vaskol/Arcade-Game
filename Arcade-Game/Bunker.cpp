58d9ea46dd8bfce4ee73db14384397f3089bdc3c
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:35:03 +0300

    Update Bunker.cpp

--- Arcade-Game/Bunker.cpp
+++ Arcade-Game/Bunker.cpp
@@ -1,12 +1,10 @@
 #include "Bunker.h"
 #include "config.h"
-#include "game.h"		//<--  (στο GameObject ???)
+#include "game.h"		
 
-
-Bunker::Bunker(const Game& myGame, float bun_pos_x, float bun_pos_y, float bun_top_side_size  /* <-- FIX !! */)
-	:GameObjCollRect(myGame, bun_pos_x, bun_pos_y, bun_top_side_size /* <-- FIX !! */)
+Bunker::Bunker(const Game& myGame, float bun_pos_x, float bun_pos_y, float bun_top_side_size)
+	:GameObjCollRect(myGame, bun_pos_x, bun_pos_y, bun_top_side_size)
 {
-	//init();		//** CHECK
 	size = 80.0f;
 }
 
@@ -15,66 +13,62 @@ Bunker::~Bunker()
 }
 
 
-void Bunker::update()		//θα αλλάζει η κατάστασή του ανάλογα με τη ζημιά (το ποσοστό ζωής). Με κάποιο enum "state" ίσως.
+void Bunker::update()		
 {
-	GameObject::update();		//** FIX
+	GameObject::update();		
 }
 
-void Bunker::draw()			//++ τη σκιά.
+void Bunker::draw()			
 {
-	graphics::Brush br;					//** CHECK (local method variable).
-	br.outline_opacity = 0.0f;			//na mi sxediazei to perigramma (isxuei gia όλα τα σχετικά με το Bunker).
+	graphics::Brush br;					
+	br.outline_opacity = 0.0f;			
 
-	graphics::setScale(1.0f, 1.0f);			//για τη μεγέθυνση-scale (ίδια τα height??). Αν το κάνω 1 φορά στην αρχή του παιχν. ισχύει για ΟΛΟ το παιχν.
+	graphics::setScale(1.0f, 1.0f);			
 
 	/*Draw bunker's shadow*/
-	GameObject::drawSpriteTexture(br, 0.5f, draw_gobj_png.get_path_to_shadow());			//** CHECK brush.
-	graphics::drawRect(pos_x - 10.0f, pos_y + 5.0f, size * draw_gobj_png.get_png_dims_ratio(), size, br);		//image dimensions: 66*140.
-
+	GameObject::drawSpriteTexture(br, 0.5f, draw_gobj_png.get_path_to_shadow());			
+	graphics::drawRect(pos_x - 10.0f, pos_y + 5.0f, size * draw_gobj_png.get_png_dims_ratio(), size, br);		
 	
-	graphics::resetPose();		//επαναφέρει και τον προσανατ. και το scale.
-	br.texture = "";			//το αναιρώ (κενό string). 
+	graphics::resetPose();		
+	br.texture = "";		
 
-	/*Draw bunker*/									//** FIX: οι επιμέρους εντολές -> σε μια μέθοδο (CHECK και στις άλλες draw).
+	/*Draw bunker*/									
 	float bun_life = Bunker::getRemainingLife();
-	const char* bun_path_to_sprite = "";			// initialize char array.
+	const char* bun_path_to_sprite = "";			
 
-	if ( (0.7f < bun_life) && (bun_life <= 1.0f) )		//**CHECK: με status??
-		bun_path_to_sprite = draw_gobj_png.get_path_to_sprite();			//** FIX.
+	if ( (0.7f < bun_life) && (bun_life <= 1.0f) )		
+		bun_path_to_sprite = draw_gobj_png.get_path_to_sprite();			
 	else if( (0.5f < bun_life) && (bun_life <= 0.7f) ) 						
 		bun_path_to_sprite = "bunkers\\bunker_2x1_bottom_destroyed.png";
 	else if( (0.3f < bun_life) && (bun_life <= 0.5f) )	
 		bun_path_to_sprite = "bunkers\\bunker_1x2_top_destroyed.png";
 	else if( (0.0f < bun_life) && (bun_life <= 0.3f) )		
 		bun_path_to_sprite = "bunkers\\bunker_2x1_top_destroyed.png";
-	//else if(bun_life <= 0.0f)		//** FIX: όταν είναι 0 η ζωή -> δεν θα έχει τίποτα.		
-		//bun_path_to_object = "";
+		
 
-	GameObject::drawSpriteTexture(br, 1.0f, bun_path_to_sprite);			//** CHECK brush.
+	GameObject::drawSpriteTexture(br, 1.0f, bun_path_to_sprite);		
 	graphics::drawRect(pos_x, pos_y, size * draw_gobj_png.get_png_dims_ratio(), size, br);		//image dimensions: 66*140.
 
-	graphics::resetPose();		//επαναφέρει και τον προσανατ. και το scale.
-	br.texture = "";			//το αναιρώ (κενό string). 
+	graphics::resetPose();		
+	br.texture = "";			
 
 
 
 	if (game.getDebugMode())
 	{
-		graphics::Brush br;				//** CHECK
+		graphics::Brush br;			
 
-		br.outline_opacity = 1.0f;		//** (same brush) CHECK.
-		br.texture = "";				//** CHECK
+		br.outline_opacity = 1.0f;		
+		br.texture = "";			
 		GameObject::drawBrushFill(br, 0.2f, 0.7f, 0.4f, 0.3f, false);
 
-		Rectangle bun_rect = getCollisionHull(draw_gobj_png.get_png_dims_ratio(), 1.0f);			//** CHECK inheritance return type --> διάλ.	
+		Rectangle bun_rect = getCollisionHull(draw_gobj_png.get_png_dims_ratio(), 1.0f);			
 		graphics::drawRect(bun_rect.cx, bun_rect.cy, bun_rect.rec_width, bun_rect.rec_height, br);
 	}
 }
 
-void Bunker::init()		//** CHECK
+void Bunker::init()		
 {
-	//size = 70.0f;	
-	//one_collision_dead = false;
 	width_factor = draw_gobj_png.get_png_dims_ratio();
 	height_factor = 1.0f;
 }
