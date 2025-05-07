804619aa2333f8d7bb8e22e8f19ef570ef116240
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:31:30 +0300

    Update Movable.h

--- Arcade-Game/Movable.h
+++ Arcade-Game/Movable.h
@@ -3,12 +3,12 @@
 #include "graphics.h"
 
 
-class Movable			//κλάση, όχι διεπαφή. (Μικρή)->εδώ και όχι σε άλλο αρχείο ??
+class Movable			
 {
 protected:
 	float speed = 0.0f;
 	bool move_to_east = true;		// true: if the movable object moves from the left to the right side of canvas [player, playerBullet]. 
-									// false: if the movable object moves from the right to the left side of canvas [meteorite, alien, alienBullet].
+						// false: if the movable object moves from the right to the left side of canvas [meteorite, alien, alienBullet].
 public:
 	void set_move_to_east(bool d) { move_to_east = d; }
 	float move() const;
