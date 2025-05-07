1eed7b6ca018b9bde927bd8069283bb0c02e507b
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:44:16 +0300

    Update enemy.cpp

--- Arcade-Game/enemy.cpp
+++ Arcade-Game/enemy.cpp
@@ -1,17 +1,17 @@
-#include "game.h"		//<--  (στο GameObject ???)
+#include "game.h"		
 #include "Enemy.h"
-#include "config.h"		//<--
-#include "util.h"		//<--
+#include "config.h"		
+#include "util.h"		
 #include <random>
 
 
 Enemy::Enemy(const Game& mygame)
-	: GameObjCollDisk(mygame, pos_x, pos_y, size)	//καλεί τον constr του προγόνου. Οι τιμές αρχικοποιούνται στην init().
+	: GameObjCollDisk(mygame, pos_x, pos_y, size)	
 {
 
 }
 
-Enemy::~Enemy()		//Δεν έχω κάτι να καταστρέψω, άρα ο destructor μου δεν κάνει κάτι.
+Enemy::~Enemy()		
 {
 }
 
@@ -22,17 +22,3 @@ void Enemy::update()
 
 	pos_x -= move()/50.0f;
 }
-
-/*
-void Enemy::draw()		
-{
-	
-}*/
-
-
-/*
-Shape* Enemy::getCollisionHull() const			//** CHECK inheritance return type --> διάλ. (++ const)		// explanation !!
-{
-	Disk* en_disk = new Disk(pos_x, pos_y, size * 0.45f);			// 0.42f για τον μετεωρ -> παράμετρος.
-	return en_disk;
-}*/
