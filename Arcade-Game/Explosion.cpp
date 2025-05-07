af7b53c885baf3f18b1f130e71fb17008fc0ce48
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:35:43 +0300

    Update Explosion.cpp

--- Arcade-Game/Explosion.cpp
+++ Arcade-Game/Explosion.cpp
@@ -3,9 +3,9 @@
 
 
 Explosion::Explosion(const Game& mygame, float bul_pos_x, float bul_pos_y)
-	:GameObject(mygame, bul_pos_x, bul_pos_y, size)		// size = 30.0f; (το μέγεθος με το οποίο αρχικ/κε η σφαίρα στην init() )
+	:GameObject(mygame, bul_pos_x, bul_pos_y, size)		
 {
-	init();			//** CHECK (tutorial).
+	init();		
 }
 
 Explosion::~Explosion()
@@ -16,36 +16,26 @@ Explosion::~Explosion()
 void Explosion::update()
 {
 	GameObject::update();
-
-	/*
-	while (active) {
-		if (expl_sinf >= 1.0f) {
-			active = false; //kati allo
-		}
-		break;
-	}*/
 }
 
 void Explosion::draw()
 {
-	graphics::setOrientation(rotation);		// <--
-	brush.outline_opacity = 0.0f;		// <--
+	graphics::setOrientation(rotation);		
+	brush.outline_opacity = 0.0f;		
 
 	for(int i=0; i<10; i++){
 		/*Draw explosion.*/
 		float expl_sinf = sinf(graphics::getGlobalTime() / 2000.0f) / 10.0f;;
 		
-		GameObject::drawSpriteTexture(brush, 0.5f, draw_gobj_png.get_path_to_sprite());			//** CHECK brush.
+		GameObject::drawSpriteTexture(brush, 0.5f, draw_gobj_png.get_path_to_sprite());			
 		graphics::drawRect(pos_x, pos_y, size * i * expl_sinf, size * i * expl_sinf, brush);
 
 		graphics::resetPose();
 	}
 }
 
-void Explosion::init()		/* CHECK -> tutorial */
+void Explosion::init()		
 {
-	size = 50 + 100 * rand0to1();			//Αρχική θέση μετεωρίτη.
-	speed = 0.3f;
-
-	//brush.outline_opacity = 0;		
+	size = 50 + 100 * rand0to1();			
+	speed = 0.3f;	
 }
