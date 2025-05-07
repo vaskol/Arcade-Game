f8460044713e4a111562ae3000b8e186e098c0c4
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:45:45 +0300

    Update gameObject.h

--- Arcade-Game/gameObject.h
+++ Arcade-Game/gameObject.h
@@ -4,35 +4,27 @@
 #include "graphics.h"	
 #include "PNG.h"
 
-
-
-
 class GameObject	
 {
 protected:
 	const class Game& game;		
 	float pos_x, pos_y;
-	float size;  //= 0.0f;		
+	float size; 	
 	float life = 1.0f;		
 	bool active = true;		
-	
-
-
 	PNG draw_gobj_png;		// the png needed for each drawable game object.
 
 public:
 	GameObject(const class Game& mygame, float gobj_pos_x, float gobj_pos_y, float gobj_size);
-	virtual ~GameObject() {}		//virtual destructor, so objects of subclasses can be deleted correctly/successfully.
-
-	//** FIX
+	virtual ~GameObject() {}		
 	virtual void update();
 	virtual void draw()=0;
 	virtual void init()=0;
 
-	float getPosX() const { return pos_x; }	//inline (ìéêñÞ)
-	float getPosY() const { return pos_y; }	//inline (ìéêñÞ)
+	float getPosX() const { return pos_x; }	
+	float getPosY() const { return pos_y; }	
 
-	float getSize() const { return size; }	//inline (ìéêñÞ)
+	float getSize() const { return size; }	
 
 	bool isActive() const { return active; }		
 
