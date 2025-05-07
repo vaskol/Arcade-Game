bd120b06a73235c86d7e2db3954debcc48c636ac
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:48:10 +0300

    Update enemy.h

--- Arcade-Game/enemy.h
+++ Arcade-Game/enemy.h
@@ -3,17 +3,11 @@
 #include "GameObjCollDisk.h"
 #include "Movable.h"
 
-
 class Enemy : public GameObjCollDisk, public Movable
 {
 protected:					
-
-		
-
 public:					
 	Enemy(const class Game& mygame);
 	virtual	~Enemy();		
-
 	virtual void update() override;		
-
 };
