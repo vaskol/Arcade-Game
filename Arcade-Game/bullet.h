bfed15adbe5b80dd4de40f6627511b6188282691
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:48:49 +0300

    Update bullet.h

--- Arcade-Game/bullet.h
+++ Arcade-Game/bullet.h
@@ -1,19 +1,15 @@
 #pragma once
 
-#include <stdlib.h>			//** CHECK
+#include <stdlib.h>		
 #include "GameObjCollRect.h"
 #include "Movable.h"
 
-
 class Bullet : public GameObjCollRect, public Movable
-{
-		
+{		
 	graphics::Brush br;		
-
 public:
 	Bullet(const class Game& mygame, float bul_pos_x, float bul_pos_y /*, float bul_size*/);
 	~Bullet();
-
 	void update() override;
 	void draw() override;
 	void init() override;
