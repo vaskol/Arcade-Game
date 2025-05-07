7f4a770bb30c6252bdf19ff3aa87479d6cdab8b3
Vaso Kollia <62697199+vaskol@users.noreply.github.com>
2025-05-07 17:43:53 +0300

    Update bullet.h

--- Arcade-Game/bullet.h
+++ Arcade-Game/bullet.h
@@ -7,12 +7,8 @@
 
 class Bullet : public GameObjCollRect, public Movable
 {
-	//pos_x = 100, pos_y = CANVAS_HEIGHT/2;
-	//size = 30;		// στη draw()->drawRect
-	//bool active = true;
-	//float speed = 0.3f;			//στην init(): speed=0.4f
-
-	graphics::Brush br;			//** CHECK -> στην GameObject ??
+		
+	graphics::Brush br;		
 
 public:
 	Bullet(const class Game& mygame, float bul_pos_x, float bul_pos_y /*, float bul_size*/);
